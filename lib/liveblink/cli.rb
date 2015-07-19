fb960f0821b373321b85b5e39411e75f91d0499b
Nicolas Oman <nickomang@gmail.com>
2015-07-18 20:50:48 -0500

    added test module to see if modules work, and guess what, they do

--- lib/liveblink/cli.rb
+++ lib/liveblink/cli.rb
@@ -1,37 +1,27 @@
 require 'thor'
 require 'liveblink/cli/fav'
+require 'liveblink/cli/test'
 
 module LiveBlink
   class Wrapper < Thor
-    desc "hello NAME", "This will greet you"
-    long_desc <<-HELLO_WORLD
+    # make quality optional
+    # change watch to just be nameless
 
-    `hello NAME` will print out a message to the person of your choosing.
-
-    Brian Kernighan actually wrote the first "Hello, World!" program 
-    as part of the documentation for the BCPL programming language 
-    developed by Martin Richards. BCPL was used while C was being 
-    developed at Bell Labs a few years before the publication of 
-    Kernighan and Ritchie's C book in 1972.
-
-    http://stackoverflow.com/a/12785204
-    HELLO_WORLD
-    option :upcase
-    def hello( name )
-      greeting = "Hello, #{name}"
-      greeting.upcase! if options[:upcase]
-      puts greeting
-    end
-
-    desc "watch [STREAM_NAME]", "Watches stream associated with url"
-    def watch (url)
+    desc "watch [STREAM_NAME] [QUALITY]", "Watches stream associated with url"
+    option :scrub
+    def watch (url, quality)
       twitch = "http://www.twitch.tv/#{url}"
-      string = "livestreamer #{twitch} best"
+      string = "livestreamer #{twitch} #{quality}"
+      if options[:scrub]
+        string += " scrub_string"
+      end
       puts string
     end
 
-
     desc "fav [COMMANDS]", "Favorites control module"
     subcommand "fav", LiveBlink::CLI::Fav
+
+    desc "test {module}", "test control module"
+    subcommand "test", LiveBlink::CLI::Test
   end
 end
